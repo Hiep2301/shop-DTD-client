e1d4875812221da1ea758d0b4bbb95948fc4e578
Hiep0203 <ngotranhiep2015@gmail.com>
2024-06-21 22:54:51 +0700

    work with Image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,29 @@
 import Card from "@/app/Card";
+import Image from "next/image";
 
 export default function Home() {
-  return <Card />;
+  return (
+    <>
+      <Card />
+      <main>
+        <h1 className="text-4xl text-center">Hello world</h1>
+        <Image
+          src={"/images/suffer.png"}
+          alt=""
+          width={500}
+          height={200}
+          quality={100}
+        />
+        <Image
+          src={
+            "https://images.pexels.com/photos/5511553/pexels-photo-5511553.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          }
+          alt=""
+          width={500}
+          height={200}
+          quality={100}
+        />
+      </main>
+    </>
+  );
 }
